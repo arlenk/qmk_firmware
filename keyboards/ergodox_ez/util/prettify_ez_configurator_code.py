e2f33918c201143d1a10323eb09d67b883810ffb
arlenk <arlen.khodadadi@gmail.com>
2018-05-31 23:27:49 -0400

    support multi-line layout definitions

--- keyboards/ergodox_ez/util/prettify_ez_configurator_code.py
+++ keyboards/ergodox_ez/util/prettify_ez_configurator_code.py
@@ -26,49 +26,197 @@ LAYOUT_FORMATS = {
 }
 
 
-def parse_keymap_line(line):
+def get_layout_definitions(lines):
     """
-    Parse a single keymap definition line
+    Extra and parse layer definitions from source lines
 
-    :param line:
-    :return:
+    :param lines: List[str]
+    :return: dict[str->dict]
+
     """
-    # keymap definition line will look like:
+    layer_definition_lines = _extract_layer_definitions(lines)
+    layer_definition_lines = _group_layer_definitions(layer_definition_lines)
+
+    layers = dict()
+    for line in layer_definition_lines:
+        layer_name, layer_definition = _parse_layer_line(line)
+        layers[layer_name] = layer_definition
+
+    return layers
+
+
+def _group_layer_definitions(lines):
+    """
+    Group (potentially) multi line layer definitions into one line per defition
+
+    :param lines: List[str]
+        lines of layer definition source code (from extract_layer_definitions)
+
+    :return: List[str]
+        one line per layer
+
+    """
+    layers = []
+
+    # each layer is defined with a line (or multiple lines) like
+    #   '[MDIA] = LAYOUT_ergodox(',
+    #      ...
+    #    'KC_TRNS, KC_TRNS, KC_WBAK',
+    #    '),'
+    #
+    # we'll split based on layer names (ie, the [MDIA] above)
+    # since layer definitions have commas in them, we can't use ending comma
+    # to find eny of each layer so we'll just keep going till the next
+    # layer name (or till we reach the end of lines)
+    definition = []
+    for iline, line in enumerate(lines):
+        line = line.strip()
+        if line.startswith("["):
+            # found start of a layer definition.. if we were already
+            # in an existing layer definition, append to knwon layers
+            # and start over
+            if len(definition):
+                layers.append(definition)
+                definition = []
+
+        definition.append(line)
+
+    # make sure to catch last layer definition
+    if len(definition):
+        layers.append(definition)
+
+    layers = ["".join(definition) for definition in layers]
+
+    return layers
+
+
+def _extract_layer_definitions(lines):
+    """
+    Find all layout definitions in lines of source code
+
+    :param lines: List[str]
+        lines of source code
+    :return: List[str]
+        lines that contain layer definitions
+
+    """
+    LAYERS_DEFINITIONS_START = "const uint16_t PROGMEM keymaps"
+
+    # first, find start of layer definitions.  These are defined in lines like:
+    #
+    # const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    #
+    #   [0] = KEYMAP(KC_EQUAL,KC_1,KC_2,KC_3, ...
+    #
+    #   [1] = KEYMAP(KC_ESCAPE,KC_F1,KC_F2, ...
+    #   ...
+    # };
+    for iline, line in enumerate(lines):
+        line = line.strip()
+        if line.startswith(LAYERS_DEFINITIONS_START):
+            # for now.. assume start of definitions (and end of definitions)
+            # are on their own lines.  No reason this has to be true, but it's much
+            # easier to parse if we assume it's true and all files in qmk layouts
+            # are formatted like this
+            if not line.endswith("{"):
+                raise ValueError("can only handle layer definitions that are on separate lines")
+            break
+    else:
+        raise ValueError("could not find start of layer definitions.  "
+                         "Looking for: {}".format(LAYERS_DEFINITIONS_START))
+
+    lines = lines[(iline+1):]
+
+    # now keep reading till we get to closing "};"
+    layer_definitions = []
+
+    in_multi_line_comment = False
+    for iline, line in enumerate(lines):
+        line = line.strip()
+
+        # ignore multi-line comments
+        if line.startswith("/*"):
+            in_multi_line_comment = True
+
+        if in_multi_line_comment:
+            if line.endswith("*/"):
+                in_multi_line_comment = False
+            continue
+
+        # ignore single line comments
+        if "//" in line:
+            line = line.split('//')[0].strip()
+
+        # we shouldn't find any more open brackets (start of declaration)
+        # so throw error if we do
+        if "{" in line:
+            raise ValueError("error parsing line, not expecting nested declaration: {}".format(line))
+
+        # are we at end of declaration?
+        if "};" in line:
+            if len(line) > 2:
+                raise ValueError("expecting declaration end to be its own line: {}".format(line))
+            break
+
+        # if not.. this line must be part of declaration
+        if len(line) > 0:
+            layer_definitions.append(line)
+
+    return layer_definitions
+
+
+def _parse_layer_line(line):
+    """
+    Parse a single layer definition line
+
+    :param line: str
+    :return: str, dict
+        layer name and dict of key values
+
+    """
+    LAYER_DEFINITION_START = ('KEYMAP(', 'LAYOUT(', 'LAYOUT_ergodox(')
+
+    # layer definition line will look like:
     #      " [0] = KEYMAP(KC_EQUAL,KC_1,KC_2,KC_3,KC_4,KC_5, ... RCTL_T(KC_ENTER)), "
+    # KEYMAP can also be LAYOUT or LAYOUT_ergodox
+    #
     # we wil extract layer name ("0" in this case and key definitions (as a dict)
     line = line.strip()
     line = line.split("=")
     if len(line) != 2:
-        raise ValueError("could not parse keymap line: {}".format(line))
+        raise ValueError("could not parse layer line: {}".format(line))
 
     layer_name, layer_keys = line
     layer_name = layer_name.strip()
-    layer_keys = layer_keys.strip().upper()
+    layer_keys = layer_keys.strip()
 
     if not (layer_name.startswith('[') and layer_name.endswith(']')):
         raise ValueError("error parsing layer name: {}".format(layer_name))
     layer_name = layer_name[1:-1]
 
-    if not (layer_keys.startswith("KEYMAP(") and layer_keys.endswith("),")):
+    if not (layer_keys.startswith(LAYER_DEFINITION_START) and layer_keys.endswith("),")):
         raise ValueError("error parsing layer keys: {}".format(layer_keys))
 
     print("parsing layer: {}".format(layer_name))
-    layer_keys = layer_keys.replace("KEYMAP(", "", 1)
+    for token in LAYER_DEFINITION_START:
+        if token in layer_keys:
+            layer_keys = layer_keys.replace(token, "", 1)
+            break
     layer_keys = layer_keys[:-2]
 
     # layer keys is now a string like:
     #    'KC_EQUAL,KC_1,KC_2,KC_3,KC_4,KC_5,... RCTL_T(KC_ENTER)'
     # we'll convert this to a dict where key is the "KEY_NUMBER" (0 to 80)
-    keymap = dict()
+    layer_definition = dict()
     for ikey, key in enumerate(_split_key_definitions(layer_keys)):
-      keymap[ikey] = key
+        layer_definition[ikey] = key.strip()
 
-    return layer_name, keymap
+    return layer_name, layer_definition
 
 
 def _split_key_definitions(line):
     """
-    Split a comma seperated line of key definitions (which may have embedded commas)
+    Split a comma separated line of key definitions (which may have embedded commas)
 
     :param line: str
     :return: generator of strs
@@ -96,7 +244,8 @@ def _split_key_definitions(line):
 
         token.append(char)
 
-    yield "".join(token)
+    token = "".join(token)
+    yield token.strip()
 
 
 def fill_format(fmt, keymap):
