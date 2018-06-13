50da5d1b448e8e34b91d14db1f87f12b23219415
arlenk <arlen.khodadadi@gmail.com>
2018-06-12 23:09:11 -0400

    add support for tap dance functions

---
+++ keyboards/ergodox_ez/keymaps/arlenk/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_ARLENK_H
+#define CONFIG_ARLENK_H
+
+// standard ergodox heard
+#include "../../config.h"
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
+
+#endif
